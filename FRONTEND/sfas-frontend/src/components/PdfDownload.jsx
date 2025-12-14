fe460439479caba937f608538666c2c252ed2518
praju455 <5434223prajwaln@gmail.com>
2025-12-14 17:39:53 +0530

    Update PdfDownload.jsx

--- FRONTEND/sfas-frontend/src/components/PdfDownload.jsx
+++ FRONTEND/sfas-frontend/src/components/PdfDownload.jsx
@@ -3,11 +3,14 @@ import { useTranslation } from "react-i18next";
 export default function PdfDownload() {
   const { t } = useTranslation();
 
+  const API = import.meta.env.VITE_BACKEND_URL;
+
   return (
     <div className="text-center">
       <a
-        href="http://127.0.0.1:5000/api/report"
+        href={`${API}/api/report`}
         target="_blank"
+        rel="noopener noreferrer"
         className="bg-green-700 text-white px-4 py-2 rounded"
       >
         {t("download")}
@@ -15,4 +18,3 @@ export default function PdfDownload() {
     </div>
   );
 }
-
