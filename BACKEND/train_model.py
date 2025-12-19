ce4d0c9eec8661aa55b0a927c3debd5e61ba022f
praju455 <5434223prajwaln@gmail.com>
2025-12-19 19:50:39 +0530

    Update train_model.py

--- BACKEND/train_model.py
+++ BACKEND/train_model.py
@@ -1,1 +1,34 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import pickle
+
+# Load dataset
+data = pd.read_csv("Crop_recommendation.csv")
+
+# Features & target
+X = data.drop("label", axis=1)
+y = data["label"]
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = RandomForestClassifier(
+    n_estimators=150,
+    random_state=42
+)
+model.fit(X_train, y_train)
+
+# Accuracy
+accuracy = accuracy_score(y_test, model.predict(X_test))
+print("Model Accuracy:", accuracy)
+
+# Save model
+pickle.dump(model, open("crop_model.pkl", "wb"))
+
+print("✅ Model trained & saved as crop_model.pkl")
 
