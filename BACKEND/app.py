7295ebdde5c86d045bb62c71ca309888ea7d7897
praju455 <5434223prajwaln@gmail.com>
2025-12-19 18:32:29 +0530

    Add dotenv support to load environment variables
    Load environment variables from a .env file.

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -2,6 +2,12 @@ from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from pymongo import MongoClient
 import io
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 app = Flask(__name__)
 CORS(app)
